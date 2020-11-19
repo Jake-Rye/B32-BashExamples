03389fc51fa3a9ef636ef81b4eab66b3ba9111c6
Jacques <a@b.c>
2020-11-19 15:32:35 -0500

    exercices sur echo

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,20 @@
-#!/bin/bash
+/#!/bin/bash
 
+# Ceci est un commnetaire
+
+echo "salut Tony"
+#ou
+echo salut Tony
+#ou
+echo 'salut Tony'
+
+echo -n "Vive "
+echo "Linux"
+# Va faire ecrire Vieve Linux sur la meme ligne sans saut de ligne
+
+echo -e "1\n2\n3\n4" #-e interprète les métacaractères et \n fait saut de ligne
+
+read -p "My name is : " nom 
+#read = cin, -p "mon message" >> variable, ici nom qui a été déclarée
+
+echo "tchik ka tchick ka tchick" $nom
